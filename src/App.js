de7703ad9160265e6a9d3c9d87393ec91b2e8848
jashan <jashandeep.singh@antiersolutions.com>
2025-01-22 18:19:51 +0530

    react welcome page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+    const styles = {
+        textAlign: 'center',
+        marginTop: '20%',
+    };
+
+    return (
+        <div style={styles}>
+            <h1>Hello, World!</h1>
+        </div>
+    );
+}
+
+export default App;
